03a0926b111c3077cdfd5c2e56962544e518c833
Hannes Kneiding <hannes.kneiding@creative-quantum.eu>
2020-12-14 13:05:54 +0100

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    initialiseParticles();
+};
+
+function initialiseParticles() {
+    Particles.init({
+        selector: '.background',
+        color: ['#b3b3b3', '#757575', '#363636'],
+        maxParticles: 100,
+        speed : 0.25,
+        connectParticles: true,
+        sizeVariations : 5,
+        minDistance: 100,
+        // options for breakpoints
+        responsive: [
+            {
+            breakpoint: 768,
+            options: {
+                maxParticles: 80,
+                sizeVariations : 4,
+                }
+            }, {
+            breakpoint: 425,
+            options: {
+                maxParticles: 50,
+                sizeVariations : 4,
+                }
+            }, {
+            breakpoint: 320,
+            options: {
+                maxParticles: 0, // disables particles.js
+                }
+            }
+        ] 
+        });
+    }
