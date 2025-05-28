081926157746d1ea2626c4d475b42c54e958476b
Hannes Kneiding <k.1996@live.de>
2025-05-28 10:57:29 +0200

    add icons and links

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,33 +6,10 @@ function initialiseParticles() {
     Particles.init({
         selector: '.background',
         color: ['#b3b3b3', '#757575', '#363636'],
-        maxParticles: 100,
-        speed : 0.25,
+        maxParticles: Math.floor( 0.0001 * window.screen.width * window.screen.height ),
+        speed : 0.3,
         connectParticles: true,
         sizeVariations : 5,
         minDistance: 100,
-        // options for breakpoints
-        responsive: [
-            {
-            breakpoint: 768,
-            options: {
-                maxParticles: 80,
-                sizeVariations : 4,
-                minDistance: 80,
-                }
-            }, {
-            breakpoint: 425,
-            options: {
-                maxParticles: 40,
-                sizeVariations : 4,
-                minDistance: 60,
-                }
-            }, {
-            breakpoint: 320,
-            options: {
-                maxParticles: 0, // disables particles.js
-                }
-            }
-        ] 
-        });
-    }
+    });
+}
